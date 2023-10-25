8f8b664705c7e68a8bacc002fa5b0c2ae6437d5b
M0H1K4N <nejer911@gmail.com>
2023-10-25 13:59:50 +0400

    adding some js atributes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import atom from './assets/react-core-concepts.png'
+import logo from "./logo.svg";
+import "./App.css";
+import atom from "./assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  return (
+    <header>
+      <img src={atom} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
 
 function App() {
   return (
     <div>
-      <header>
-        <img src={atom} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
